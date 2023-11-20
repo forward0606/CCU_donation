b85b79b4f3ed9d1d34bb904821661a262e3f4199
IHCA <83703795+IHCA@users.noreply.github.com>
2023-11-21 01:39:34 +0800

    Update form and list

--- public/progress.js
+++ public/progress.js
@@ -1,4 +1,4 @@
-var currentProgress = 0;
+var currentProgress = 25;
 
 function plusProgress() {
     var progressBar = document.getElementById("progress-bar");
@@ -8,6 +8,7 @@ function plusProgress() {
         progressBar.style.width = currentProgress + "%";
     }
 
+
 }
 
 function minusProgress() {
@@ -16,5 +17,47 @@ function minusProgress() {
     if (currentProgress > 0) {
         currentProgress -= 25;
         progressBar.style.width = currentProgress + "%";
+    }
+}
+
+
+function plusPage(pageNum) {
+
+    var page1 = document.getElementById("page1");
+    var page2 = document.getElementById("page2");
+    var page3 = document.getElementById("page3");
+
+    var currentPage = document.getElementById(pageNum);
+
+    if (currentPage === page1) {
+        page1.style.display = "none";
+        page2.style.display = "block";
+        plusProgress();
+    }
+    else if(currentPage === page2){
+        page2.style.display = "none";
+        page3.style.display = "block";
+        plusProgress();
+    }
+}
+
+
+function minusPage(pageNum) {
+
+    var page1 = document.getElementById("page1");
+    var page2 = document.getElementById("page2");
+    var page3 = document.getElementById("page3");
+
+    var currentPage = document.getElementById(pageNum);
+
+    if (currentPage === page2) {
+        page1.style.display = "block";
+        page2.style.display = "none";
+        minusProgress();
+    }
+    else if(currentPage === page3){
+        page2.style.display = "block";
+        page3.style.display = "none";
+        minusProgress();
     }
 }
