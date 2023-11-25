a1e1807e2acfdf4b21481dc4e30cdcc96e1c41ea
IHCA <83703795+IHCA@users.noreply.github.com>
2023-11-25 23:35:42 +0800

    finish progress bar ^^

--- public/progress.js
+++ public/progress.js
@@ -1,43 +1,27 @@
-var currentProgress = 25;
-
-function plusProgress() {
-    var progressBar = document.getElementById("progress-bar");
-    
-    if (currentProgress < 100) {
-        currentProgress += 25;
-        progressBar.style.width = currentProgress + "%";
-    }
-
-
-}
-
-function minusProgress() {
-    var progressBar = document.getElementById("progress-bar");
-    
-    if (currentProgress > 0) {
-        currentProgress -= 25;
-        progressBar.style.width = currentProgress + "%";
-    }
-}
-
+var currentProgress = 0;
 
 function plusPage(pageNum) {
 
     var page1 = document.getElementById("page1");
     var page2 = document.getElementById("page2");
     var page3 = document.getElementById("page3");
+    var list = document.querySelectorAll('.progress_list li');
 
     var currentPage = document.getElementById(pageNum);
 
     if (currentPage === page1) {
         page1.style.display = "none";
         page2.style.display = "block";
-        plusProgress();
+        list[currentProgress].classList.add('active_rec');
+        currentProgress = currentProgress + 1;
+        list[currentProgress].classList.add('active_cir');  
         scrollToTop();
     }else if(currentPage === page2){
         page2.style.display = "none";
         page3.style.display = "block";
-        plusProgress();
+        list[currentProgress].classList.add('active_rec');
+        currentProgress = currentProgress + 1;
+        list[currentProgress].classList.add('active_cir');
         scrollToTop();
     }
 }
@@ -48,18 +32,23 @@ function minusPage(pageNum) {
     var page1 = document.getElementById("page1");
     var page2 = document.getElementById("page2");
     var page3 = document.getElementById("page3");
+    var list = document.querySelectorAll('.progress_list li');
 
     var currentPage = document.getElementById(pageNum);
 
     if (currentPage === page2) {
         page1.style.display = "block";
         page2.style.display = "none";
-        minusProgress();
+        list[currentProgress].classList.remove('active_cir');
+        currentProgress = currentProgress - 1;
+        list[currentProgress].classList.remove('active_rec');
         scrollToTop();
     }else if(currentPage === page3){
         page2.style.display = "block";
         page3.style.display = "none";
-        minusProgress();
+        list[currentProgress].classList.remove('active_cir');
+        currentProgress = currentProgress - 1;
+        list[currentProgress].classList.remove('active_rec');
         scrollToTop();
     }
 }
