2a67e0730a01a872de6cb544ea125144a388807e
IHCA <83703795+IHCA@users.noreply.github.com>
2023-11-27 20:14:24 +0800

    update progress bar

--- public/progress.js
+++ public/progress.js
@@ -1,5 +1,6 @@
 var currentProgress = 0;
 
+
 function plusPage(pageNum) {
 
     var page1 = document.getElementById("page1");
@@ -12,16 +13,22 @@ function plusPage(pageNum) {
     if (currentPage === page1) {
         page1.style.display = "none";
         page2.style.display = "block";
+        list[currentProgress].classList.remove('active_cir');
         list[currentProgress].classList.add('active_rec');
+        list[currentProgress].classList.add('active_complete'); 
         currentProgress = currentProgress + 1;
-        list[currentProgress].classList.add('active_cir');  
+        list[currentProgress].classList.add('active_cir');
+        // list[currentProgress].classList.add('active_complete');    
         scrollToTop();
     }else if(currentPage === page2){
         page2.style.display = "none";
         page3.style.display = "block";
+        list[currentProgress].classList.remove('active_cir');
         list[currentProgress].classList.add('active_rec');
+        list[currentProgress].classList.add('active_complete');
         currentProgress = currentProgress + 1;
         list[currentProgress].classList.add('active_cir');
+        // list[currentProgress].classList.add('active_complete'); 
         scrollToTop();
     }
 }
@@ -41,14 +48,18 @@ function minusPage(pageNum) {
         page2.style.display = "none";
         list[currentProgress].classList.remove('active_cir');
         currentProgress = currentProgress - 1;
+        list[currentProgress].classList.add('active_cir');
         list[currentProgress].classList.remove('active_rec');
+        list[currentProgress].classList.remove('active_complete');
         scrollToTop();
     }else if(currentPage === page3){
         page2.style.display = "block";
         page3.style.display = "none";
         list[currentProgress].classList.remove('active_cir');
         currentProgress = currentProgress - 1;
+        list[currentProgress].classList.add('active_cir');
         list[currentProgress].classList.remove('active_rec');
+        list[currentProgress].classList.remove('active_complete');
         scrollToTop();
     }
 }
