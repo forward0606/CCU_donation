f87d0231af26b268d876fdcbbbd3eba1e42320f6
reki9185 <decimo9185@gmail.com>
2023-12-01 17:16:27 +0800

    alert window

--- public/progress.js
+++ public/progress.js
@@ -9,7 +9,7 @@ function plusPage(pageNum) {
     var list = document.querySelectorAll('.progress_list li');
 
     var currentPage = document.getElementById(pageNum);
-
+    //if (currentPage === page1 && (validate(pageNum))) {
     if (currentPage === page1) {
         page1.style.display = "none";
         page2.style.display = "block";
@@ -20,6 +20,7 @@ function plusPage(pageNum) {
         list[currentProgress].classList.add('active_cir');
         // list[currentProgress].classList.add('active_complete');    
         scrollToTop();
+    //}else if(currentPage === page2 && (validate(pageNum))){
     }else if(currentPage === page2){
         page2.style.display = "none";
         page3.style.display = "block";
@@ -33,6 +34,56 @@ function plusPage(pageNum) {
     }
 }
 
+function validate(pageNum) {
+    var currentPage = document.getElementById(pageNum);
+    var page1 = document.getElementById("page1");
+
+    var name = document.getElementById("donation_name").value;
+    var person_id =document.getElementById("donation_person_id").value;
+    var email =document.getElementById("donation_email").value;
+    var phone =document.getElementById("phone").value;
+    var flag = 0;
+    alert(flag);
+
+    var Message = document.getElementById('message');
+    Message.textContent = "";
+
+
+    if(currentPage == page1){
+        if(name.length <= 0){
+            Message.textContent.append("請輸入姓名\n");
+            flag++;
+        }
+        if(person_id.length <= 0){
+            Message.textContent.append("請輸入身分證字號\n");
+            flag++;
+        }
+        if(email.length <= 0){
+            Message.textContent.append("請輸入 Email\n");
+            flag++;
+        }
+        if(phone.length <= 0){
+            Message.textContent.append("請輸入電話號碼\n");
+            flag++;
+        }
+    }
+    if(flag != 0){
+        showAlert();
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function showAlert() {
+    var Alert = document.getElementById('alert_window');
+    Alert.style.display = 'block';
+}
+
+function closeAlert() {
+    var Alert = document.getElementById('customAlert');
+    Alert.style.display = 'none';
+}
 
 function minusPage(pageNum) {
 
